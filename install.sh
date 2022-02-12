0bbbe83951f0ef005295e5c5e8c5b996bb80e8af
smittytone <smittytone@icloud.com>
2022-02-12 12:06:04 +0000

    Add submodule; app install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change is if you have changed the name of your CircuitPython drive
+TARGET_PICO="/Volumes/CIRCUITPY"
+
+# Copy required libraries
+cp ./HT16K33-Python/ht16k33.py "${TARGET_PICO}/Lib/ht16k33.py"
+cp ./HT16K33-Python/ht16k33segment14.py "${TARGET_PICO}/Lib/ht16k33segment14.py"
+
+# Copy application code
+cp ./boot.py "${TARGET_PICO}/boot.py"
+cp ./code.py "${TARGET_PICO}/code.py"
